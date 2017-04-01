3b246de67fbbf8a24d0942d645a7a74eca3ee0b8
gyyin <1451830100@qq.com>
2017-04-01 15:09:41 +0800

    Update webpack.config.js

--- blog/webpack.config.js
+++ blog/webpack.config.js
@@ -14,8 +14,8 @@ var config = {
          "extentions": ["","js"]//当requrie的模块找不到时，添加这些后缀
     },
     "output": {
-        "path": "./client/dist/js",
-        "filename": "[name].js"
+        "path": "./client/dist",
+        "filename": "js/[name].js"
     },
     "watch": true,
     "module": {
@@ -25,16 +25,21 @@ var config = {
             loader: "babel"
         }, {
             test: /\.(scss|sass|css)$/,
-            loader: ExtractTextPlugin.extract({fallbackLoader: "style-loader", loader: "css-loader!postcss-loader!sass-loader"})
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader!postcss-loader", {
+                publicPath: '../'
+            })
         }, {
             test: /\.(png|jpg|jpng|eot|ttf)$/,
-            loader: 'url-loader?limit=8192&name=../images/[name].[ext]'
+            loader: 'url-loader?limit=8192&name=images/[name].[ext]'
         }]
     },
     postcss: [autoprefixer],
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
-        new ExtractTextPlugin("../css/index.css"),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            filename: "js/bundle.js"
+        }),
+        new ExtractTextPlugin("css/index.css"),
         new webpack.DefinePlugin({
             "process.env": { 
                 NODE_ENV: JSON.stringify("production") 
